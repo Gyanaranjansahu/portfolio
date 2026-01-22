3b2d54d2a33fbb49dbe1e0801479386e0e9b90cc
gyana <gyana9619@gmail.com>
2026-01-22 11:45:53 +0530

    'hii'

--- src/folders/navbar.jsx
+++ src/folders/navbar.jsx
@@ -1,19 +1,28 @@
 import logo from "../assets/logo.jpg"
 import { FaRegMessage } from "react-icons/fa6";
 import './nav.css'
+import { useState } from "react";
 export default function Navbar() {
+    const[display,setDisplay]=useState(true);
     return(
         <nav>
             <div className="container">
-                <div style={{display:'flex', justifyContent:'center'}}><h3>Gyana's <span style={{color:"yellow"}}>Portfolio</span></h3></div>
-                <ul style={{display:'flex', justifyContent:'space-evenly',alignItems:'center', listStyleType:'none'}}>
+                <h3>Gyana's <span style={{color:"yellow"}}>Portfolio</span></h3>
+                <ul style={{display:'flex', justifyContent:'space-evenly',alignItems:'center', listStyleType:'none', transition:'all 0.2s'}} className={display?"hide":''} >
                     <li><a onClick={()=>{document.getElementById("home").scrollIntoView({behavior:"smooth"})}} className="tag" href="#home">Home</a></li>
                     <li><a onClick={()=>{document.getElementById("about").scrollIntoView({behavior:"smooth"})}} className="tag" href="#about">About</a></li>
                     <li> <a  className="tag" href="#">Skills</a></li>
                     <li> <a  className="tag" href="#">Projects</a></li>
                      <li> <a  className="tag" href="#">Contact</a></li>
                 </ul>
+
             </div>
+            <div className="humber" onClick={()=>{setDisplay(!display)}}>
+                <div className="side1 check"></div>
+            <div className="side2 check"></div>
+            <div className="side3 check"></div>
+            </div>
+            
         </nav>
     )
 };
