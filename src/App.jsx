276460207675b88ecef9493b65b1305e71794005
gyana <gyana9619@gmail.com>
2026-01-24 07:38:21 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from "./folders/navbar";
 import "./app.css"
 import Content from "./folders/section1";
 import About from "./folders/section2";
+import Education from "./folders/section3";
 export default function App() {
   return(
     <div className="body">
       <Navbar/>
       <Content/>
       <About/>
+      <Education/>
     </div>
   )
 };
---
+++ src/folders/section3.jsx
@@ -0,0 +1,41 @@
+import './edu.css'
+import { RiGraduationCapLine } from "react-icons/ri";
+import { FaSchool } from "react-icons/fa";
+import { BiSolidSchool } from "react-icons/bi";
+export default function Education(){
+return(
+    <section className="education">
+        <h1>My Education</h1>
+        <div className="container">
+            <div className="sub">
+                <button>2020</button>
+                
+                <span ><h4>High School(10th Grade)   <RiGraduationCapLine/></h4></span>
+                <span>BSE Odisha</span>
+                <span>Gyana Vikash Residental School</span>
+                <span>Odisha</span>
+                <div className='line'></div>
+                Score:54%
+            </div>
+            <div className="sub">
+                   <button>2022</button>
+                <span ><h4>High Secondary(12th Grade)  <FaSchool/></h4></span>
+                <span>CHSE Odisha</span>
+                <span>Kalam institute of technology</span>
+                <span>Odisha,Berhampur</span>
+                <div className='line'></div>
+                Score:72%
+            </div>
+            <div className="sub">
+                   <button>2026</button>
+                <span ><h4>Bachelor of Technology   < BiSolidSchool/></h4></span>
+                <span>Computer Science Engineering</span>
+                <span>Kalam institute of Technology</span>
+                <span>Odisha</span>
+                <div className='line'></div>
+                Score:54%
+            </div>
+        </div>
+    </section>
+)
+}
