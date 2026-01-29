318f79569ec49b89405f5381f911fb5e63d715b0
gyana <gyana9619@gmail.com>
2026-01-29 19:55:19 +0530

    'add'

--- src/folders/section5.jsx
+++ src/folders/section5.jsx
@@ -3,7 +3,13 @@ import { MdEmail } from "react-icons/md";
 import { FaTelegramPlane } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { useState } from 'react';
-export default function Contact(params) {
+export default function Contact() {
+            function add3(){
+        return window.open('mailto:gyana9619@gmail.com') ;
+    }
+        function add1(){
+        return window.open('https://www.linkedin.com/in/gyanaranjan-sahu-6331a8333?utm_source=share_via&utm_content=profile&utm_medium=member_android') ;
+    }
     const[sub,setSub]=useState({
         name:'',
         email:''
@@ -20,13 +26,13 @@ export default function Contact(params) {
             <h2 style={{ margin: '2rem' }} >Contact Me</h2>
             <div className="con">
                 <div className="part1">
-                    <div className="email">
+                    <div className="email" onClick={()=>add3()}>
                         <span style={{ width: '70px' }}><MdEmail /></span>
                         <h4>Email</h4>
                         <p>gyana9619@gmail.com</p>
                         <p>SEND A MAIL</p>
                     </div>
-                    <div className="linkedin">
+                    <div className="linkedin" onClick={()=>add1()}>
                         <span><FaLinkedin /></span>
                         <h4>Linkedin</h4>
                         <p>Gyanaranjan Sahu</p>
