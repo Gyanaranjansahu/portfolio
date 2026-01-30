3d04156faad6f8794b2f1e66741305c031a08e54
gyana <gyana9619@gmail.com>
2026-01-30 13:44:27 +0530

    add

--- src/folders/section1.jsx
+++ src/folders/section1.jsx
@@ -6,6 +6,7 @@ import { FaInstagram } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
+import { ReactTyped } from "react-typed";
 export default function First() {
     function add1(){
         return window.open('https://www.linkedin.com/in/gyanaranjan-sahu-6331a8333?utm_source=share_via&utm_content=profile&utm_medium=member_android') ;
@@ -18,7 +19,19 @@ export default function First() {
     <section className="part" id="home">
         <div className="about" >
             <span className="name">Hello,I'm <br /> <span style={{ color: "yellow" }}>Gyana</span></span>
-            <span className="designer">Web Developer</span><br />
+            <span className="designer" style={{}}>
+                <ReactTyped
+      strings={[
+        "Web Developer",
+        "Frontend Developer",
+        "MERN STACK Developer",
+      ]}
+      typeSpeed={160}
+      backSpeed={130}
+      loop
+    >
+    </ReactTyped>
+                </span><br />
             <p id="a">i am skilled and passionate Web Developer <br />
                 with hands-on experience in building responsive and user friendly Website </p>
             <div className="div">
